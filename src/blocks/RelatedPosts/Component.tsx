dc98e78a4579878770407a65f6ab4fe9c9c5f9d9
Joe simpson <joe@egmrc.com>
2024-12-10 13:03:41 -0700

    Adjust grid gap spacing in RelatedPosts component
    Reduced the gap between grid items from `4/8` to `1` for better compactness and alignment. This change ensures a more cohesive layout, especially on smaller screens.

--- src/blocks/RelatedPosts/Component.tsx
+++ src/blocks/RelatedPosts/Component.tsx
@@ -20,7 +20,7 @@ export const RelatedPosts: React.FC<RelatedPostsProps> = (props) => {
     <div className={clsx('container', className)}>
       {introContent && <RichText content={introContent} enableGutter={false} />}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 items-stretch">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 items-stretch">
         {docs?.map((doc, index) => {
           if (typeof doc === 'string') return null
 
