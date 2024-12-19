bb11903feb5d67c2f5c5841cae1a0fa1d5a91740
Joe simpson <joe@egmrc.com>
2024-12-19 11:03:41 -0700

    Fix too many connection bug

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://loc
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental:{
+    staticGenerationRetryCount: 1,
+    staticGenerationMaxConcurrency: 2,
+    staticGenerationMinPagesPerWorker: 25
+  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
