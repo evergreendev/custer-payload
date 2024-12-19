6499ba76d0ecc93f24ead116b6c62b934458bc31
Joe simpson <joe@egmrc.com>
2024-12-19 15:14:34 -0700

    Improve font colors

--- src/blocks/ButtonGroup/Component.tsx
+++ src/blocks/ButtonGroup/Component.tsx
@@ -15,7 +15,7 @@ export const ButtonGroup: React.FC<
   return (
       <div className="bg-brand-blue border-b-[20px] border-brand-red p-4 flex flex-wrap gap-8 md:flex-row md:justify-between md:items-center pt-7 pb-20">
         <div className="container flex flex-col items-center">
-          <div className="max-w-[48rem] flex items-center">
+          <div className="max-w-[48rem] flex items-center text-white">
             {introContent && <RichText className="mb-4 prose-xl prose-h2:font-normal" content={introContent} enableGutter={false} />}
           </div>
           <div className="flex gap-8 w-full items-center">
--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -15,7 +15,7 @@ export const CallToActionBlock: React.FC<
   return (
     <div className="container">
       <div className="bg-card rounded border-border border p-4 flex flex-col gap-8 md:flex-row md:justify-between md:items-center">
-        <div className="max-w-[48rem] flex items-center">
+        <div className="max-w-[48rem] flex items-center text-white">
           {richText && <RichText className="mb-0" content={richText} enableGutter={false} />}
         </div>
         <div className="flex flex-col gap-8">
