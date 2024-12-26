7c6b89b8f475c6d625d07c30432d5196be0e52bd
Joe simpson <joe@egmrc.com>
2024-12-26 14:06:41 -0700

    Fix popups caching

--- src/collections/PopUps/component/index.tsx
+++ src/collections/PopUps/component/index.tsx
@@ -6,7 +6,7 @@ import { cookies } from 'next/headers'
 
 const PopUp = async () => {
   const cookieStore = await cookies()
-  const hidePopup = cookieStore.get('hide-popups')
+  const hidePopups = cookieStore.get('hide-popups')
   const dateObj = new Date()
 
   const payload = await getPayload({ config: configPromise })
@@ -14,96 +14,48 @@ const PopUp = async () => {
     async () => {
       return await payload.find({
         pagination: false,
-        where: hidePopup
-          ? {
-              and: [
+        where: {
+          and: [
+            {
+              or: [
                 {
-                  id: {
-                    not_in: hidePopup.value.split('|'),
-                  },
+                  startShowing: { less_than_equal: dateObj },
                 },
                 {
                   or: [
                     {
-                      startShowing: { less_than_equal: dateObj },
+                      startShowing: {
+                        exists: false
+                      }
                     },
                     {
-                      or: [
-                        {
-                          startShowing: {
-                            exists: false
-                          }
-                        },
-                        {
-                          startShowing: { equals: null },
-                        },
-                      ],
-                    },
-                  ],
-                },
-                {
-                  or: [
-                    {
-                      stopShowing: { greater_than_equal: dateObj },
-                    },
-                    {
-                      or: [
-                        {
-                          stopShowing: {
-                            exists: false
-                          }
-                        },
-                        {
-                          stopShowing: { equals: null },
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            }
-          : {
-              and: [
-                {
-                  or: [
-                    {
-                      startShowing: { less_than_equal: dateObj },
-                    },
-                    {
-                      or: [
-                        {
-                          startShowing: {
-                            exists: false
-                          }
-                        },
-                        {
-                          startShowing: { equals: null },
-                        },
-                      ],
-                    },
-                  ],
-                },
-                {
-                  or: [
-                    {
-                      stopShowing: { greater_than_equal: dateObj },
-                    },
-                    {
-                      or: [
-                        {
-                          stopShowing: {
-                            exists: false
-                          }
-                        },
-                        {
-                          stopShowing: { equals: null },
-                        },
-                      ],
+                      startShowing: { equals: null },
                     },
                   ],
                 },
               ],
             },
+            {
+              or: [
+                {
+                  stopShowing: { greater_than_equal: dateObj },
+                },
+                {
+                  or: [
+                    {
+                      stopShowing: {
+                        exists: false
+                      }
+                    },
+                    {
+                      stopShowing: { equals: null },
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
         collection: 'popUp',
         sort: 'priority',
         depth: 2,
@@ -113,11 +65,19 @@ const PopUp = async () => {
     [],
     {
       tags: ['popups'],
-    },
+      revalidate: 3600
+    }
   )
-  const data = await fetchPopUps()
+  const data = await fetchPopUps();
 
-  return <PopUpClient popUps={data.docs} />
+  const filteredPopups = data.docs.filter(doc => {
+    if (hidePopups?.value){
+      return !hidePopups.value.split("|").find(x => x === doc.id.toString())
+    }
+    return true;
+  })
+
+  return <PopUpClient popUps={filteredPopups} />
 }
 
 export default PopUp
